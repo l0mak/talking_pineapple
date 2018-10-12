4ec20facace00507e345e871ca0dc5da14c8a39a
l0mak <dimabam.bam@gmail.com>
2018-10-13 01:08:16 +0300

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,2 +1,1 @@
-try:
-    from config.config import __token__, __prefix__, __botserverid__, __greetmsg__, __selfassignrole__, __cookieJar__, __discourseAPIKey__
+from config.config import __token__, __prefix__
