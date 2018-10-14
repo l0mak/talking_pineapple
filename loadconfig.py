31681f499dfc49e8a1475338058c011ad683aef7
l0mak <dimabam.bam@gmail.com>
2018-10-15 02:09:28 +0300

    Update loadconfig.py

--- loadconfig.py
+++ loadconfig.py
@@ -4,5 +4,5 @@ except ImportError:
 	import os
 	
 from config.games import __games__, __gamesTimer__	
-from config.cogs import __bmain__, __encounters__, __test__	
+from config.cogs import __cogs__	
 from config.blacklist import __blacklist__
