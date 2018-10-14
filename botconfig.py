0da64ef96668c2cdd161ed768e7f2c66daecd0e6
l0mak <dimabam.bam@gmail.com>
2018-10-15 00:42:38 +0300

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
 	import os
 	
-#from config.games import __games__, __gamesTimer__	
+from config.games import __games__, __gamesTimer__	
 from config.cogs import __core__, __encounters__, __test__	
 from config.blacklist import __blacklist__
