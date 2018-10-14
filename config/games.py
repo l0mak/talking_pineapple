157ab3ac1447033b1254310597678ab876895136
l0mak <dimabam.bam@gmail.com>
2018-10-15 00:40:50 +0300

    Update games.py

--- config/games.py
+++ config/games.py
@@ -1,16 +1,16 @@
-import discord
+#import discord
 
-__games__ = [
-    (discord.setActivity.playing, 'с Друзьями'),
-    (discord.ActivityType.playing, 'c Фералами'),
-    (discord.ActivityType.playing, 'попробуйте %help'),
-    (discord.ActivityType.playing, 'попробуйте %info'),
-    (discord.ActivityType.playing, 'в Черепаха Дошла До Воды'),
-    (discord.ActivityType.playing, 'с Капитаном Пирожком'),
-    (discord.ActivityType.watching, 'за Ордорейдом'),
-    (discord.ActivityType.watching, 'на {members} Ананасиков'),
-    (discord.ActivityType.watching, 'за {guilds} Серверами'),
-    (discord.ActivityType.watching, "стрим Джин'Зиггоря"),
-    (discord.ActivityType.listening, 'чарующий голос Господина Флаттера')
-]
-__gamesTimer__ = 2 * 60
+#__games__ = [
+#    (discord.setActivity.playing, 'с Друзьями'),
+#    (discord.ActivityType.playing, 'c Фералами'),
+#    (discord.ActivityType.playing, 'попробуйте %help'),
+#    (discord.ActivityType.playing, 'попробуйте %info'),
+#    (discord.ActivityType.playing, 'в Черепаха Дошла До Воды'),
+#    (discord.ActivityType.playing, 'с Капитаном Пирожком'),
+#    (discord.ActivityType.watching, 'за Ордорейдом'),
+#    (discord.ActivityType.watching, 'на {members} Ананасиков'),
+#    (discord.ActivityType.watching, 'за {guilds} Серверами'),
+#    (discord.ActivityType.watching, "стрим Джин'Зиггоря"),
+#    (discord.ActivityType.listening, 'чарующий голос Господина Флаттера')
+#]
+#__gamesTimer__ = 2 * 60
