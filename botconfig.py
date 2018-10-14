251866150efa75d552db1ba483421cbc8fabe9f5
l0mak <dimabam.bam@gmail.com>
2018-10-14 23:20:06 +0300

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -1,5 +1,5 @@
 try:
-    from config.config import __token__, __prefix__
+	from config.config import __token__, __prefix__
 	from config.games import __games__, __gamesTimer__
 	from config.cogs import __core__, __encounters__, __test__
 	from config.blacklist import __blacklist__
